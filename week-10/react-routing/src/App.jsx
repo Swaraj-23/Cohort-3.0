c70defe0419338575285602a92d44482b4e47597
Swaraj Pawar <pawarswaraj2307@gmail.com>
2025-01-01 22:58:37 +0530

    changes in week-10

--- week-10/react-routing/src/App.jsx
+++ week-10/react-routing/src/App.jsx
@@ -2,14 +2,15 @@ import { StrictMode, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Chat from './Chat'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Chat/>
+    </div>
   )
 }
 
