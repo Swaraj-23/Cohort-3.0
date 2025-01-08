58bce3dc7c66e8dabc2e43f0039acaa5c2f6826f
Swaraj Pawar <pawarswaraj2307@gmail.com>
2025-01-08 20:29:10 +0530

    updated the useDebounce hook

--- week-11/useDebounce-hook/src/App.jsx
+++ week-11/useDebounce-hook/src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useRef } from 'react';
 
-function useDebounce(originalFn)
+// function useDebounce(originalFn)
+// {
+//   const currClock = useRef();
+
+//   function fn()
+//   {
+//     clearTimeout(currClock.current);
+//     currClock.current = setTimeout(originalFn, 200)
+//   }
+
+//   return fn;
+// }
+
+// function App() {
+
+//   function sendDataToBackend()
+//   {
+//     fetch("api.amazon.com/search/");
+//   }
+
+//   const debouncedFn = useDebounce(sendDataToBackend);
+
+//   return (
+//     <>
+//       <input type='text' onChange={debouncedFn}></input>
+//     </>
+//   )
+// }
+
+// export default App
+
+/* Another implementation of useDebounce hook */
+
+function useDebounce(value, delay)
 {
-  const currClock = useRef();
+  const[debouncedValue, setDebouncedValue] = useState(value);
 
-  function fn()
-  {
-    clearTimeout(currClock.current);
-    currClock.current = setTimeout(originalFn, 200)
+  useEffect(() =>{
+    const handler = setTimeout(() =>{
+      setDebouncedValue(value);
+  }, delay);
+
+  return ()=>{
+    clearTimeout(handler);
   }
+  }, [value, delay]);
 
-  return fn;
+  return debouncedValue;
 }
 
-function App() {
+function App()
+{
+  const[inputVal, setInputVal] = useState("");
+  const debouncedValue = useDebounce(inputVal, 200);
 
-  function sendDataToBackend()
-  {
-    fetch("api.amazon.com/search/");
+  function change(e){
+    setInputVal(e.target.value);
   }
 
-  const debouncedFn = useDebounce(sendDataToBackend);
+  useEffect(()=>{
+    //fetch
+    console.log("expensive operation")
+  },[debouncedValue]);
 
-  return (
+  return(
     <>
-      <input type='text' onChange={debouncedFn}></input>
+      <input type='text' onChange={change}></input>
     </>
   )
 }
